3700d739f003b4ccf6a361bf7e4bbb2f6d67de7f
pritam-samantray <samantraypritam6@gmail.com>
2026-02-10 13:09:12 +0530

    feat-3: adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import Maincomponent from './Component/Maincomponent/Maincomponent'
+import Navbar from './Component/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     <h1>My App</h1>
     <Maincomponent/>
+    
      
     </>
   )
